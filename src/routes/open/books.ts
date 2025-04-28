74aca9469261a2f119e783c52cfcdc977144cdfc
KingsGlaive42 <thomas.h.le123@gmail.com>
2025-04-27 17:05:49 -0700

    book making branch

--- src/routes/open/books.ts
+++ src/routes/open/books.ts
@@ -76,3 +76,92 @@ booksRouter.get('/:author', (request: Request, response: Response) => {
 });
 
 export { booksRouter };
+
+/**
+ * @api {post} /books Create a new book
+ * @apiName CreateBook
+ * @apiGroup Books
+ *
+ * @apiParam {String} title Full title of the book.
+ * @apiParam {String} original_title Original title of the book.
+ * @apiParam {BigInt} isbn13 ISBN-13 number of the book.
+ * @apiParam {Number} original_publication_year Year the book was first published.
+ * @apiParam {String} authors List of authors (comma separated).
+ * @apiParam {Number} average_rating Average rating of the book.
+ * @apiParam {Number} ratings_count Total number of ratings.
+ * @apiParam {Number} ratings_1 Number of 1-star ratings.
+ * @apiParam {Number} ratings_2 Number of 2-star ratings.
+ * @apiParam {Number} ratings_3 Number of 3-star ratings.
+ * @apiParam {Number} ratings_4 Number of 4-star ratings.
+ * @apiParam {Number} ratings_5 Number of 5-star ratings.
+ * @apiParam {String} image_url Link to a large image of the book.
+ * @apiParam {String} small_image_url Link to a small image of the book.
+ *
+ * @apiSuccess {Object} book The created book object.
+ * @apiSuccess {Number} book.book_id Book ID.
+ * @apiSuccess {BigInt} book.isbn13 ISBN-13 number.
+ * @apiSuccess {String} book.authors Authors of the book.
+ * @apiSuccess {Number} book.original_publication_year Year the book was first published.
+ * @apiSuccess {String} book.original_title Original title of the book.
+ * @apiSuccess {String} book.title Full title of the book.
+ * @apiSuccess {Number} book.average_rating Average rating of the book.
+ * @apiSuccess {Number} book.ratings_count Total number of ratings.
+ * @apiSuccess {Number} book.ratings_1 Number of 1-star ratings.
+ * @apiSuccess {Number} book.ratings_2 Number of 2-star ratings.
+ * @apiSuccess {Number} book.ratings_3 Number of 3-star ratings.
+ * @apiSuccess {Number} book.ratings_4 Number of 4-star ratings.
+ * @apiSuccess {Number} book.ratings_5 Number of 5-star ratings.
+ * @apiSuccess {String} book.image_url Link to a large image of the book.
+ * @apiSuccess {String} book.small_image_url Link to a small image of the book.
+ *
+ * @apiError (400 Bad Request) {String} message "Invalid input data"
+ * @apiError (500 Internal Server Error) {String} message "server error - contact support"
+ */
+booksRouter.post('/books', async (request: Request, response: Response) => {
+    const {
+        title, original_title, isbn13, original_publication_year, authors,
+        average_rating, ratings_count, ratings_1, ratings_2, ratings_3,
+        ratings_4, ratings_5, image_url, small_image_url
+    } = request.body;
+
+    // Validate the input data
+    if (!title || !original_title || !isbn13 || !original_publication_year || !authors || !image_url || !small_image_url) {
+        return response.status(400).send({
+            message: 'Invalid input data',
+        });
+    }
+
+    // Convert isbn13 to BigInt if it's not already
+    const isbnNumber = BigInt(isbn13);
+
+    // Insert the new book into the database
+    try {
+        const insertQuery = `
+            INSERT INTO books (isbn13, authors, original_publication_year, original_title, title, 
+                               average_rating, ratings_count, ratings_1, ratings_2, ratings_3, 
+                               ratings_4, ratings_5, image_url, small_image_url)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+            RETURNING book_id, isbn13, authors, original_publication_year, original_title, title, 
+                      average_rating, ratings_count, ratings_1, ratings_2, ratings_3, ratings_4, ratings_5, 
+                      image_url, small_image_url
+        `;
+        const values = [
+            isbnNumber, authors, original_publication_year, original_title, title,
+            average_rating, ratings_count, ratings_1, ratings_2, ratings_3,
+            ratings_4, ratings_5, image_url, small_image_url
+        ];
+
+        const result = await pool.query(insertQuery, values);
+
+        // Return the created book
+        response.status(201).send({
+            book: result.rows[0],
+        });
+    } catch (error) {
+        console.error('DB Query error on POST /books');
+        console.error(error);
+        response.status(500).send({
+            message: 'server error - contact support',
+        });
+    }
+});
